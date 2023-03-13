ccd3e2581ebc6cf9ed24d6770ea1c5b5ce004a51
a161120 <david.tarvin@physiciansmutual.com>
2023-03-13 15:06:33 -0500

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import {AfterViewInit, Component, ElementRef, QueryList, ViewChild, ViewChildren
 import {COURSES} from '../db-data';
 import {Course} from './model/course';
 import {CourseCardComponent} from './course-card/course-card.component';
+import { HighlightedDirective } from './directives/highlighted.directive';
 
 @Component({
   selector: 'app-root',
@@ -12,6 +13,8 @@ export class AppComponent implements AfterViewInit {
 
     courses = COURSES;
 
+    @ViewChild(CourseCardComponent, {read:HighlightedDirective} )
+    highlighted: HighlightedDirective;
 
     @ViewChildren(CourseCardComponent, {read: ElementRef})
     cards : QueryList<ElementRef>;
@@ -21,8 +24,12 @@ export class AppComponent implements AfterViewInit {
 
     }
 
-    ngAfterViewInit() {
+    onToggle(isHighlighted:boolean) {
+      console.log('isHighlighted is ' + isHighlighted);
+    }
 
+    ngAfterViewInit() {
+      console.log(this.highlighted)
     }
 
     onCourseSelected(course:Course) {
